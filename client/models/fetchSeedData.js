74240f1349edb70b63d1d24f7afe937909beb269
davidlewallen <lewallen.david@gmail.com>
2016-08-01 10:14:47 -0500

    Added Models to file structure

---
+++ client/models/fetchSeedData.js
@@ -0,0 +1,28 @@
+export const seedData = {
+  billList: [
+    {
+      company        : 'Bill - Company - 1',
+      amount         : 159.27,
+      amountPerPerson: 39.82,
+      dueDate        : new Date(2016, 7, 1)
+    },
+    {
+      company        : 'Bill - Company - 2',
+      amount         : 10.45,
+      amountPerPerson: 2.62,
+      dueDate        : new Date(2016, 7, 15),
+    },
+    {
+      company        : 'Bill - Company - 3',
+      amount         : 16,
+      amountPerPerson: 4,
+      dueDate        : new Date(2016, 7, 5)
+    },
+    {
+      company        : 'Bill - Company - 4',
+      amount         : 100,
+      amountPerPerson: 25,
+      dueDate        : new Date(2016, 7, 31)
+    }
+  ]
+}
