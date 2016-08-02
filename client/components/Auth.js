18ed064b9cb3c1dce9b30e89ef2c1d623041fa41
davidlewallen <lewallen.david@gmail.com>
2016-08-02 00:12:56 -0500

    (feat) Add Auth (Sign in/ sign out)

---
+++ client/components/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	// TODO: Implement user auth (Signin/signout)
+	render() {
+		return (
+		)
+	}
+}
