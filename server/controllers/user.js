3aca5804979c273286b6a1d178dc5359414d79f2
davidlewallen <lewallen.david@gmail.com>
2016-08-31 00:25:08 -0500

    (refactor) Remove duplicate and change post to put

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,15 +24,10 @@ router.get('/email/:userEmail', function(req, res, next) {
 
 // *** GET users by group *** //
 router.get('/group/:groupId', function(req, res, next) {
-  utils.queryHandler(Users.getUsersByGroup, req.params.groupId, req, res, next);
+  utils.queryHandler(User.getUsersByGroup, req.params.groupId, req, res, next);
 })
 
-// *** GET user data *** //
-router.get('/user/:userId', function(req, res, next) {
-  utils.queryHandler(Users.getUser, req.params.userId, req, res, next);
-})
-
-// *** POST set user's group *** //
-router.post('/setUserGroup/:groupId', function(req, res, next) {
+// *** PUT set user's group *** //
+router.put('/setGroup/:groupId', function(req, res, next) {
   utils.queryHandler(Users.setUserGroup, req.params.groupId, req, res, next);
 })
