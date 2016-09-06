55606f8b96e68ce4de1462152639fe32b57ca036
davidlewallen <lewallen.david@gmail.com>
2016-09-06 00:05:31 -0500

    (feat) Added new router for group.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/oauth', require('./authentication.js'));
 router.use('/bill', require('./bill'));
 router.use('/user', require('./user'));
+router.use('/group', require('./group'))
 
 module.exports = router;
