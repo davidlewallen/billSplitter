c2ecfa3e5916bf69b67622880da22fa6dbc83ee8
davidlewallen <lewallen.david@gmail.com>
2016-09-06 00:13:52 -0500

    (refactor) Changed back to 200 for PUT

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -3,7 +3,7 @@ module.exports = {
 }
 
 function queryHandler(query, param, req, res, next) {
-  const status = req.method === 'POST' || req.method === 'PUT' ? 201 : 200;
+  const status = req.method === 'POST' ? 201 : 200;
 
   query(param)
   .then(resp => { res.status(status).json(resp) })
